2b41a535c347cc1e504a5b7721de22050116561d
AaRai27 <raihangeorgia@gmail.com>
2020-11-03 15:29:22 +0700

    First Commit
    First Commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>KukerHut</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/open-iconic-bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.css') ?>">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/owl.carousel.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/owl.theme.default.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/magnific-popup.css') ?>">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/aos.css') ?>">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/ionicons.min.css') ?>">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-datepicker.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/jquery.timepicker.css') ?>">
+
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/flaticon.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/icomoon.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+</head>
+
+<body class="goto-here">
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+            <a class="navbar-brand" href="index.html" style="font-family: RosewoodStd-Regular;">Kuker Hut</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="oi oi-menu"></span> Menu
+            </button>
+
+            <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+                    <li class="nav-item active"><a href="product.html" class="nav-link">Product</a></li>
+                    <li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
+                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->

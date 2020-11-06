d60855ad70236b0bedccb29fe74ec36884164fdd
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-11-06 13:54:12 +0700

    Show all category in db
    Show all Products by Category doesn't work yet

--- application/views/contents/products.php
+++ application/views/contents/products.php
@@ -13,11 +13,10 @@
     	<div class="row justify-content-center">
     		<div class="col-md-10 mb-5 text-center">
     			<ul class="product-category">
-    				<li><a href="#" class="active">All</a></li>
-    				<li><a href="#">Vegetables</a></li>
-    				<li><a href="#">Fruits</a></li>
-    				<li><a href="#">Juice</a></li>
-    				<li><a href="#">Dried</a></li>
+					<li><a href="#" class="active">All</a></li>
+					<?php foreach ($category as $ct) { ?>
+    				<li><a href="<?= site_url('products/category/'), $ct->nama_kategori ?>"><?= $ct->nama_kategori ?></a></li>
+					<?php } ?>
     			</ul>
     		</div>
     	</div>

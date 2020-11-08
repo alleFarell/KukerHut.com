3c72793e694e88dff8db4d888e1304cec848b5fa
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-11-08 08:01:02 +0700

    Category Filter works BUT..
    tapi pagination nya jd gabisa dipake, need to be fixed

--- application/models/Products_model.php
+++ application/models/Products_model.php
@@ -11,8 +11,20 @@ class Products_model extends CI_model
         return $this->db->get('kategori')->result();
     }
 
-    public function getProductsByCategory($id_kategori)
+    public function getProductsByCategory($table, $id_kategori, $limit, $start)
     {
-        return $this->db->get_where('produk', ['id_kategori' => $id_kategori])->result_array();
+        return $this->db->get_where($table, array('id_kategori'=>$id_kategori), $limit, $start)->result_array();
     }
+
+    public function getProductsByCat($id_kategori)
+    {
+        return $this->db->query("select * from produk where id_kategori like '$id_kategori'")->result_array();
+    }
+
+    public function getCategoryByProduct($id_kategori)
+    {
+        return $this->db->get_where('kategori', ['id_kategori' => $id_kategori])->row('nama_kategori');
+    }
+
+
 }

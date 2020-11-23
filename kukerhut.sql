73e99b6c595e7e833349fe6d5a3994ee8a5fc71a
Mohammaddaffaharis <mohammaddaffaharis@gmail.com>
2020-11-23 13:17:33 +0700

    Update SQL

--- kukerhut.sql
+++ kukerhut.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Nov 2020 pada 06.41
+-- Waktu pembuatan: 23 Nov 2020 pada 07.14
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.2.27
 
@@ -30,18 +30,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id_admin` int(11) NOT NULL,
-  `username` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `username` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(128) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
-(4, 'omadmin', '$2y$10$9vBkzSLgJFOYFyjl2IH8yuQ0NsSoGFGoCca5QSaAsGOJ.ZOUZPyoy'),
-(5, 'afif', '$2y$10$ZFzxyUCwoKDRiEEQZ6XfQuY2EsveR2UDi2J6ysWHK1RTTrQEHSKie'),
-(6, 'daffa', '$2y$10$O.UmoqAkgVyV1U1/.0rbZOakKH0sVYSsjxTni55X3CBG3nLZfG8Uy');
+(1, 'ray', '$2y$10$HdyiRfFPSg9/6ACcdBaWjOEYXOy1SFqusV3/2ZhdkZOfZCowg4TI6'),
+(2, 'daffa', '$2y$10$3LxFnILoVGaM.a/ciJXnSOUt92vKtTtcPBB903Jo/jAG8rPO7LDnK');
 
 -- --------------------------------------------------------
 
@@ -60,8 +59,8 @@ CREATE TABLE `iklan` (
 --
 
 INSERT INTO `iklan` (`id_iklan`, `foto_iklan`, `nama_iklan`) VALUES
-(8, '2011.jpg', 'romadon'),
-(10, 'bg_3.jpg', 'HUT RI');
+(1, 'bg_31.jpg', 'HUT RI'),
+(2, 'about.jpg', 'idul pitri');
 
 -- --------------------------------------------------------
 
@@ -96,10 +95,9 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 CREATE TABLE `produk` (
   `id_produk` int(11) NOT NULL,
   `id_kategori` int(11) NOT NULL,
-  `nama_kategori` varchar(128) NOT NULL,
   `nama_produk` varchar(128) NOT NULL,
   `harga_produk` varchar(128) NOT NULL,
-  `foto_produk` varchar(128) DEFAULT NULL,
+  `foto_produk` varchar(128) NOT NULL,
   `deskripsi_produk` text NOT NULL,
   `editor` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -108,8 +106,8 @@ CREATE TABLE `produk` (
 -- Dumping data untuk tabel `produk`
 --
 
-INSERT INTO `produk` (`id_produk`, `id_kategori`, `nama_kategori`, `nama_produk`, `harga_produk`, `foto_produk`, `deskripsi_produk`, `editor`) VALUES
-(2, 2, 'Cookies', 'Brown Sugar Cookies', 'Rp. 20000 - 30000', 'download.jpeg', 'cookies dengan taburan brown sugar', 'daffa');
+INSERT INTO `produk` (`id_produk`, `id_kategori`, `nama_produk`, `harga_produk`, `foto_produk`, `deskripsi_produk`, `editor`) VALUES
+(13, 2, 'Brown Sugar Cookies', 'Rp. 20000 - 30000', 'download.jpeg', 'Cookies dengan taburan gula merah', 'daffa');
 
 --
 -- Indexes for dumped tables
@@ -148,25 +146,25 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `iklan`
 --
 ALTER TABLE `iklan`
-  MODIFY `id_iklan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_iklan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)

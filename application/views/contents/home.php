44adc40fa0930576cbd61b9442afb568bb3d7b61
X4D0 <46711970+X4D0@users.noreply.github.com>
2020-11-24 18:22:48 +0700

    Home - Category Showcase

--- application/views/contents/home.php
+++ application/views/contents/home.php
@@ -76,23 +76,23 @@
             <div class="col-md-8">
                 <div class="row">
                     <div class="col-md-6 order-md-last align-items-stretch d-flex">
-                        <div class="category-wrap-2 ftco-animate img align-self-stretch d-flex" style="background-image: url(<?= base_url('assets/images/category.jpg') ?>);">
+                        <div class="category-wrap-2 ftco-animate img align-self-stretch d-flex" style="background-image: url(<?= base_url('assets/images/kategori.jpg') ?>);">
                             <div class="text text-center">
-                                <h2>Vegetables</h2>
-                                <p>Protect the health of every home</p>
-                                <p><a href="#" class="btn btn-primary">Shop now</a></p>
+                                <h2>Cake and Cookies</h2>
+                                <p>Choose the best Joy</p>
+                                <p><a href="<?= base_url('kukerhut/products') ?>" class="btn btn-primary">Shop now</a></p>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/category-1.jpg') ?>);">
+                        <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/kategori-1.jpg') ?>);">
                             <div class="text px-3 py-1">
-                                <h2 class="mb-0"><a href="#">Fruits</a></h2>
+                                <h2 class="mb-0"><a href="<?= base_url(); ?>Kukerhut/category/1">Cake</a></h2>
                             </div>
                         </div>
-                        <div class="category-wrap ftco-animate img d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/category-2.jpg') ?> );">
+                        <div class="category-wrap ftco-animate img d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/kategori-2.jpg') ?> );">
                             <div class="text px-3 py-1">
-                                <h2 class="mb-0"><a href="#">Vegetables</a></h2>
+                                <h2 class="mb-0"><a href="<?= base_url(); ?>Kukerhut/category/2">Cookies</a></h2>
                             </div>
                         </div>
                     </div>
@@ -100,14 +100,14 @@
             </div>
 
             <div class="col-md-4">
-                <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/category-3.jpg') ?>);">
+                <div class="category-wrap ftco-animate img mb-4 d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/kategori-3.jpg') ?>);">
                     <div class="text px-3 py-1">
-                        <h2 class="mb-0"><a href="#">Juices</a></h2>
+                        <h2 class="mb-0"><a href="<?= base_url(); ?>Kukerhut/category/3">Dessert</a></h2>
                     </div>
                 </div>
-                <div class="category-wrap ftco-animate img d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/category-4.jpg') ?>);">
+                <div class="category-wrap ftco-animate img d-flex align-items-end" style="background-image: url(<?= base_url('assets/images/kategori-4.jpg') ?>);">
                     <div class="text px-3 py-1">
-                        <h2 class="mb-0"><a href="#">Dried</a></h2>
+                        <h2 class="mb-0"><a href="<?= base_url(); ?>Kukerhut/category/4">Daily Cake</a></h2>
                     </div>
                 </div>
             </div>
